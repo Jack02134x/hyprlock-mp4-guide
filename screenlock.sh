1f3d44331ab09b1ce0a64e48229e3a3665bbf03c
Jack <brooooooo0707@gmail.com>
2025-12-14 10:30:58 +0530

    fix

--- screenlock.sh
+++
@@ -1,7 +0,0 @@
-# enter the live wallpaper you want also if you want then you can just enter a pic and it'll work
-wallpaper=breathe.mp4   # This is just an example wallpaper
-
-#don't worry about this variable it'll do
-current=$(hyprctl activeworkspace -j | jq '.id')
-
-mpvpaper -vs -o "no-audio loop" --layer overlay eDP-1 $wallpaper & hyprlock; pkill mpvpaper
